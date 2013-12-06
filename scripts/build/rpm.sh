33b8594068392047d698e416adf1a442730d04c7
pahagon <paulo.ahagon@gmail.com>
2013-12-06 14:47:05 -0200

    Add spec for build rpm package

---
+++ scripts/build/rpm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+__project_dir=${1:-"$(cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd)"}
+__rpm_specfile=${2:-"${__project_dir}/baby-api/script/build/baby-api.spec"}
+__rpm_version=${3:-$(git for-each-ref refs/tags/* --format='%(tag)' --sort='taggerdate' | grep -v jenkins | tail -n1)}
+__rpm_dist=${4:-"1"}
+__rpmbuild_bin=$(which rpmbuild)
+
+$__rpmbuild_bin -ba $__rpm_specfile --define "version $__rpm_version" --define "__project_dir $__project_dir" --define "dist $__rpm_dist"
+
